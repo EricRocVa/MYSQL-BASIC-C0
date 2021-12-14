54d822a916c769948f6259a87a5bd5aaec48b5d5
EricRocVa <2105269@talnet.nl>
2021-12-14 09:30:13 +0100

    mysql opdracht 2 af

--- 01-Databases/Opdracht01-database-import/antwoorden.sql
+++ 01-Databases/Opdracht01-database-import/antwoorden.sql
@@ -1,1 +1,7 @@
 -- Taak01 - haal de tabel movies op:
+select * FROM movies;
+select jaar2015 FROM jaar2015;
+select country jaar2016 FROM movies;
+select region jaar2015 FROM movies;
+select region score jaar2015 FROM movies;
+select score rank jaar2016 FROM movies;
