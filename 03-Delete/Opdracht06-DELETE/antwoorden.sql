772e0d44636e5c40ff1acf526d430519ea450f08
EricRocVa <2105269@talnet.nl>
2022-02-14 10:18:47 +0100

    opdracht 06 - delete af

--- 03-Delete/Opdracht06-DELETE/antwoorden.sql
+++ 03-Delete/Opdracht06-DELETE/antwoorden.sql
@@ -1,9 +1,15 @@
--- Opdracht 1 
-
+-- Opdracht 1
+DELETE FROM `players` WHERE name = 'H. Kane'
 -- Opdracht 2 
 
+DELETE FROM `players` WHERE club = 'Willem II'
 -- Opdracht 3
 
+DELETE FROM `players` WHERE club = 'Tottenham Hotspur' AND nationality = 'England'
 -- Opdracht 4
 
+DELETE FROM `players` WHERE age >= 23
 -- Opdracht 5
+DELETE FROM `players` WHERE id = 59000000 OR 165000 
+
+(202126, 'H. Kane ',  'England ', 'Tottenham Hotspur '),
